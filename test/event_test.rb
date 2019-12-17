df1b6e415be22f1c5591bed7fa1341f5468893c0
David Atkins <atki1080@gmail.com>
2019-12-17 09:15:13 -0700

    Add test case for attributes

--- test/event_test.rb
+++ test/event_test.rb
@@ -3,9 +3,15 @@ require "minitest/pride"
 require "./lib/event"
 
 class EventTest < Minitest::Test
+  def setup
+    @event = Event.new("Curling", [24, 30, 18, 20, 41])
+  end
   def test_it_exists
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_instance_of Event, @event
+  end
 
-    assert_instance_of Event, event
+  def test_it_has_attributes
+    assert_equal "Curling", @event.name
+    assert_equal [24, 30, 18, 20, 41], @event.ages
   end
 end
