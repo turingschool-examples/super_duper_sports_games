5f6c3b1f4e0efa148cba277e4a1309d4252c5f8c
Pierce Alworth <pbalworth@gmail.com>
2019-12-17 09:38:39 -0700

    Initial commit

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,18 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+#step1
+step_1_sum = 0
+step_1 = ages.each {|age|step_1_sum += age.to_f}
 
-# When you find the standard deviation, print it out
+#step2
+step_2_count = ages.count.to_f
+
+#step3
+step_3_average = (step_1_sum.to_f / step_2_count.to_f)
+
+#step4
+step_4_start = ages.map {|age| age - step_3_average}
+step_4 = step_4_start.map {|number| number.round(2)}
+require "pry"; binding.pry
+
+#step5
