b4074081eb21223b623b43bb7b314595849375b5
David Atkins <atki1080@gmail.com>
2019-12-17 09:11:39 -0700

    Add test_it_exists test case

--- test/event_test.rb
+++ test/event_test.rb
@@ -3,5 +3,9 @@ require "minitest/pride"
 require "./lib/event"
 
 class EventTest < Minitest::Test
+  def test_it_exists
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
 
+    assert_instance_of Event, event
+  end
 end
