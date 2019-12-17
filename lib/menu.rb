12582191538eae27c393dadc58e927dd17ca6867
Nathan Keller <keller.nathan@gmail.com>
2019-12-17 11:07:08 -0700

    Refactor methods but still not storing local variable to events array

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,11 +7,15 @@ class Menu
     def start
       puts "Hello. Welcome to Super Sports Games!"
       puts "Please enter the year for the games you want to work with:"
-      year = gets.chomp
+      @year = gets.chomp
       puts "Thank You."
       puts "What is the name of the event you wish to create?"
-      event_name = gets.chomp.capitalize
+      @event_name = gets.chomp.capitalize
       puts "Thank You."
+      players
+    end
+
+    def players
       puts "What are the ages of the players you wish to create?(Total of 5)"
       puts "Player 1"
       player_age_1 = gets.chomp.to_i
@@ -23,18 +27,23 @@ class Menu
       player_age_4 = gets.chomp.to_i
       puts "Player 5"
       player_age_5 = gets.chomp.to_i
-      player_array = [player_age_1, player_age_2, player_age_3, player_age_4, player_age_5]
+      @player_array = [player_age_1, player_age_2, player_age_3, player_age_4, player_age_5]
       puts "Thank You."
-      event1 = Event.new(event_name, player_array)
-      game = Games.new(year)
-      game.add_event(event1)
-      puts "#{game.summary}"
+      report
+    end
+
+    def report
+      event = Event.new(@event_name, @player_array)
+      game = Games.new(@year)
+      game.add_event(event)
+      puts "#{game.event_summary(event)}"
       puts "Would you like to add another event?"
       again = gets.chomp.upcase
       if again == "YES"
         start
       else
-        puts "#{game.all_events_summary}"
+        puts "Summery of all Games:"
+        puts "#{game.summary}"
       end
     end
 end
