23b14568e20de4507250f2bb8f79f0ae64687959
Elom Amouzou <elom1990@yahoo.com>
2019-12-17 09:30:58 -0700

    Create standard deviationg calculation

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,15 @@
 ages = [24, 30, 18, 20, 41]
 
 # Your code here for calculating the standard deviation
+def standard_deviation(ages)
+  a_sum = ages.sum
+  a_count = ages.count
+  a_mean = (a_sum / a_count.to_f)
+  a_minus_mean = ages.map {|age| (age - a_mean).round(2)}
+  squared = a_minus_mean.map {|num| (num * num).round(3)}
+  squared_sum = a_minus_mean_squared.sum
+  squared_sum_div_by_count = squared_sum/a_count
+  std_dev = Math.sqrt(squared_sum_div_by_count).round(2)
+end
 
 # When you find the standard deviation, print it out
