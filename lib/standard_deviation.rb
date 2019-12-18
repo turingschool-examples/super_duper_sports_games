009faadec40d48e55d4c33cacf81931bfd3beb0f
Chase.Gaddis <cgaddis36@gmail.com>
2019-12-18 15:33:27 -0700

    I believe this is the correct repo

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,24 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+ages.sum
 
-# When you find the standard deviation, print it out
+ages.length
+ave_subtracted = []
+squared = []
+
+average = ages.sum / ages.length.to_f
+
+ages.each do |age|
+  ave_subtracted << (age - average).round(1)
+end
+
+ave_subtracted.each do |ave|
+  squared << (ave ** 2).round(2)
+end
+
+
+mega = squared.sum / ages.length.to_f
+
+stdev = Math.sqrt(mega)
+
+@stdev = stdev.round(2)
